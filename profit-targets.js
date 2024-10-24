570e78849040bd25fdf8b80d35559c49186aea04
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2024-10-24 15:34:37 +1030

    Implementation using sets

--- profit-targets.js
+++ profit-targets.js
@@ -1,5 +1,23 @@
-function stockPairs(stocksProfits, target) {
+function stockPairs(stocksProfit, target) {
+  const pairs = []
   
+  // const minStockProfit = Math.min(stocksProfit)
+  let stocksProfitSet = new Set(stocksProfit)
+  
+  let seenSet = new Set()
+  
+  for (const stockProfit of stocksProfitSet) {
+    if (seenSet.has(stockProfit)) {
+      continue
+    }
+    if (stocksProfitSet.has(target - stockProfit)) {
+      pairs.push([stockProfit, target - stockProfit])
+      seenSet.add(stockProfit)
+      seenSet.add(target - stockProfit)
+    }
+  }
+  
+  return pairs.length
 }
 
 module.exports = {
