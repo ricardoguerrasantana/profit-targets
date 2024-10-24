13a8189921366e05c888c3cf5cf815b643872aee
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2024-10-24 15:33:07 +1030

    Update test

--- profit-targets.test.js
+++ profit-targets.test.js
@@ -4,5 +4,5 @@ test("Sample case 0 test", () => {
   stocksProfit = [1, 3, 46, 1, 3, 9]
   target = 47
 
-  expect(stockPairs(stocksProfit, target)).toStrictEqual([1, 46])
+  expect(stockPairs(stocksProfit, target)).toStrictEqual(1)
 })
