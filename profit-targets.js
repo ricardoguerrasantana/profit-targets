6859f6b52d8711bd4cf414e3193d00dc2929e928
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2024-10-24 16:53:53 +1030

    Modify algorithm to consider edge case of test case 2

--- profit-targets.js
+++ profit-targets.js
@@ -1,23 +1,14 @@
 function stockPairs(stocksProfit, target) {
-  const pairs = []
-  
-  // const minStockProfit = Math.min(stocksProfit)
-  let stocksProfitSet = new Set(stocksProfit)
-  
-  let seenSet = new Set()
-  
-  for (const stockProfit of stocksProfitSet) {
-    if (seenSet.has(stockProfit)) {
-      continue
-    }
-    if (stocksProfitSet.has(target - stockProfit)) {
-      pairs.push([stockProfit, target - stockProfit])
-      seenSet.add(stockProfit)
-      seenSet.add(target - stockProfit)
+  let seenSets = new Set()
+
+  for (let i = 0; i < stocksProfit.length; i++) {
+    for (let j = i + 1; j < stocksProfit.length; j++) {
+      if (stocksProfit[i] + stocksProfit[j] === target) {
+        seenSets.add([stocksProfit[i], stocksProfit[j]].sort((a, b) => a - b).join(','))
+      }
     }
   }
-  
-  return pairs.length
+  return seenSets.size
 }
 
 module.exports = {
