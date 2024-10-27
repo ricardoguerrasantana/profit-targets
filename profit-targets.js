7a893b85df456f3cbac7af9a4579a8b57574767d
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2024-10-27 15:29:33 +1030

    Tide up and comment stockParis function

--- profit-targets.js
+++ profit-targets.js
@@ -1,17 +1,20 @@
 function stockPairs(stocksProfit, target) {
-  let seen = new Set()
-  let used = new Set()
+  let seen = new Set() // To store all seen profits
+  let used = new Set() // To track profits already used in pairs
   let distinctPairsCount = 0
 
   for (let i = 0; i < stocksProfit.length; i++) {
     const profit = stocksProfit[i]
-    const difference = target - profit;
-    if (seen.has(difference) && !used.has(difference) && !used.has(profit)) {
-      distinctPairsCount++
-      used.add(difference)
+    const difference = target - profit
+
+    // Check if the complement exists in seen and both values haven't been used
+    if (seen.has(difference) && !used.has(profit) && !used.has(difference)) {
+      distinctPairsCount++ // Found a valid pair
+      used.add(difference) // Mark both as used
       used.add(profit)
     }
-    seen.add(profit)
+
+    seen.add(profit) // Mark this profit as seen
   }
 
   return distinctPairsCount
